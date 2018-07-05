ee59708e05f52f16c8ee5129cbffde665c7e405c
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 01:06:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,7 @@ class Application
         end
       else
         resp.write "Your cart is empty"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
