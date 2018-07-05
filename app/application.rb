a2c05772741b35b703b8d9ae0a7b5a5638325391
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 01:06:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,9 +13,11 @@ class Application
       end
     elsif req.path.match(/cart/)
       if @@cart =! []
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      else
+        resp.write "Your cart is empty"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
