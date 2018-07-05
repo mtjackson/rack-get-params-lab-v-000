5503c66b0a809b6c3812f0140627adb48df56a90
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-05 01:04:11 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,8 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
+      *
+    end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
